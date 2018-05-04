148c13e981967eb514b0a3df79fa7c929e6d562e
Vik Kalkat <vikramjit@clearbridgemobile.com>
2018-05-04 13:59:20 -0400

    added sign up and sign in functionality. also added passwoed encryption using bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
+var bcrypt = require('bcrypt');
 
 var app = express();
 
@@ -47,6 +48,36 @@ app.put('/meows/remove', function(req, res){
     if (!err){
       return res.send();
     }
+  });
+});
+
+app.post('/users', function(req, res){
+  db.collection('users', function(err, usersCollection){
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(req.body.password, salt, function(err, hash){
+        var newUser = {
+          username : req.body.username,
+          password: hash
+        };
+        usersCollection.insert(newUser, {w:1}, function(){
+          return res.send();
+        });
+      });
+    });
+  });
+});
+
+app.put('/users/signin', function(req, res){
+  db.collection('users', function(err, usersCollection){
+    usersCollection.findOne({username: req.body.username}, function(err, user){
+      bcrypt.compare(req.body.password, user.password, function(err, result) {
+        if(result){
+          return res.send();
+        }else {
+          return res.status(400).send();
+        }
+      });
+    });
   });
 });
 
